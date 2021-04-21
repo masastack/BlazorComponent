61ef46256dad60171673dbbf60bcd3e1d748c8e7
wangzengxing <1625322170@qq.com>
2021-04-21 09:02:05 +0800

    feat:新增ListItemIcon

---
+++ src/Component/BlazorComponent/Components/Nav/BListItemIcon.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public abstract partial class BListItemIcon : BDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
