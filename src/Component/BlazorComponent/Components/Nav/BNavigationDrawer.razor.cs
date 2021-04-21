498583cbcf7c42806a855119caea0bd74432e400
wangzengxing <71120280+wangzengxing@users.noreply.github.com>
2021-04-21 13:50:53 +0800

    Feature/nav (#15)
    * feat:初步完成NavigationDrawer
    
    * feat:新增list
    
    * feat:初步完成导航功能
    
    * feat:新增ListItemIcon
    
    * refactor:优化代码
    
    * fix:解决命名冲突

---
+++ src/Component/BlazorComponent/Components/Nav/BNavigationDrawer.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorComponent
+{
+    public abstract partial class BNavigationDrawer
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        protected CssBuilder ContentCssBuilder { get; } = new CssBuilder();
+
+        protected CssBuilder BorderCssBuilder { get; } = new CssBuilder();
+
+        public virtual List<BListItem> ListItems { get; set; }
+
+        public virtual void Select(BListItem selectItem)
+        {
+
+        }
+    }
+}
