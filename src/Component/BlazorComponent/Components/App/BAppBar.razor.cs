190cbae2ed6d4fff7a32e41bd8cd61c56a8726e9
wangzengxing <1625322170@qq.com>
2021-04-21 11:01:46 +0800

    refactor:优化代码

---
+++ src/Component/BlazorComponent/Components/App/BAppBar.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorComponent.Components.Core.CssProcess;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public abstract partial class BAppBar : BDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected CssBuilder ContentCssBuilder { get; } = new CssBuilder();
+
+        protected StyleBuilder ContentStyleBuilder { get; } = new StyleBuilder();
+    }
+}
