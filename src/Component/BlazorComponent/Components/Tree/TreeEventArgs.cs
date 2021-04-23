e6193815108c2d8e2c276c231881b90f6db7f702
zhurong <zhurong@masastack.com>
2021-04-23 11:04:00 +0800

    feat: tree

---
+++ src/Component/BlazorComponent/Components/Tree/TreeEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorComponent
+{
+    public class TreeEventArgs<TItem> : EventArgs
+    {
+        public TreeEventArgs() { }
+        public TreeEventArgs(BTree<TItem> Btree) { BTree = Btree; }
+        public TreeEventArgs(BTree<TItem> Btree, BTreeItem<TItem> node) { BTree = Btree; Node = node; }
+
+        public TreeEventArgs(BTree<TItem> Btree, BTreeItem<TItem> node, MouseEventArgs originalEvent) { BTree = Btree; Node = node; OriginalEvent = originalEvent; }
+
+        public BTree<TItem> BTree { get; set; }
+        public BTreeItem<TItem> Node { get; set; }
+        public MouseEventArgs OriginalEvent { get; set; }
+    }
+}
