5e25300f6d1ede79538456f2d2c38b8b0a895760
caoyouxian <capdiem@live.com>
2021-04-28 21:11:28 +0800

    feat(select): 🆕 Add Select component

---
+++ src/Component/BlazorComponent/Components/Select/BSelect.razor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorComponent
+{
+    public partial class BSelect<TItem> : BDomComponentBase
+    {
+        protected CssBuilder ControlCssBuilder = new ();
+        protected CssBuilder SlotCssBuilder = new ();
+        protected CssBuilder SelectSlotCssBuilder = new ();
+        protected CssBuilder LabelCssBuilder = new ();
+        protected StyleBuilder LabelStyleCssBuilder = new ();
+        protected CssBuilder SelectorCssBuilder = new ();
+        protected CssBuilder SelectedCssBuilder = new ();
+        protected CssBuilder SelectInputCssBuilder = new ();
+        protected CssBuilder SelectArrowCssBuilder= new ();
+        protected CssBuilder SelectArrowIconCssBuilder = new ();
+        protected CssBuilder HitCssBuilder = new ();
+
+        protected CssBuilder ListCssBuilder = new ();
+
+        protected bool _visible;
+        protected bool _focused;
+        protected string _icon;
+
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public bool Dense { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        public bool Filled { get; set; }
+
+        [Parameter]
+        public bool Outlined { get; set; }
+
+        [Parameter]
+        public bool Solo { get; set; }
+
+        public string HitMessage { get; set; }
+
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        [Parameter]
+        public Func<TItem, string> ItemText { get; set; }
+
+        [Parameter]
+        public Func<TItem, string> ItemValue { get; set; }
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; }
+
+        protected RenderFragment SelectArrowContent { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected virtual void HandleOnBlur(FocusEventArgs args)
+        {
+            _focused = false;
+        }
+
+        protected virtual void Click(MouseEventArgs args)
+        {
+            _focused = true;
+            _visible = true;
+        }
+
+        public void SetVisible(bool visible)
+        {
+            _visible = visible;
+            InvokeStateHasChanged();
+        }
+
+        public void SetSelected(TItem value)
+        {
+            Text = ItemText != null
+                ? ItemText.Invoke(value)
+                : value.ToString();
+
+            Value = ItemValue != null
+                ? ItemValue.Invoke(value)
+                : value.ToString();
+        }
+    }
+}
---
+++ src/Component/BlazorComponent/Components/Select/BSelectOption.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public partial class BSelectOption<T> : BDomComponentBase
+    {
+        [CascadingParameter]
+        protected BSelect<T> SelectWrapper { get; set; }
+
+        [Parameter]
+        public T Value { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
