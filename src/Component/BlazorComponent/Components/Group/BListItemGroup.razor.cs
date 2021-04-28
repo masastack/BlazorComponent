fd91f34cd8b81d414617d9249d91e2f8c907d3b7
caoyouxian <capdiem@live.com>
2021-04-28 21:01:40 +0800

    feat(list): 🆕 Add ListItemGroup component

---
+++ src/Component/BlazorComponent/Components/Group/BListItemGroup.razor.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public partial class BListItemGroup : BDomComponentBase
+    {
+        [Parameter]
+        public string Value { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        public async Task ChangeValue(string key)
+        {
+            Value = key;
+            await InvokeStateHasChangedAsync();
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
--- src/Component/BlazorComponent/Components/List/BListItemTitle.razor.cs
+++ src/Component/BlazorComponent/Components/List/BListItemTitle.razor.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace BlazorComponent
 {
-    public abstract partial class BListItemTitle:BDomComponentBase
+    public partial class BListItemTitle : BDomComponentBase
     {
         [Parameter]
         public RenderFragment ChildContent { get; set; }
