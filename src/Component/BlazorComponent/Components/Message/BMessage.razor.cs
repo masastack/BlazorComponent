a2f54d9767f3847258eea41a4ff51c0f10c0fd40
caoyouxian <capdiem@live.com>
2021-04-28 21:11:04 +0800

    feat(message): 🆕 Add Message component

---
+++ src/Component/BlazorComponent/Components/Message/BMessage.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public partial class BMessage : BDomComponentBase
+    {
+        protected CssBuilder WrapperCssBuilder = new();
+        protected CssBuilder MessageCssBuilder = new();
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
