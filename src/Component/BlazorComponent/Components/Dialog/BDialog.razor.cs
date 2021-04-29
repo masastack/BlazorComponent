c45a39eb0862458a35139b06da437b8aa0509e2a
caoyouxian <capdiem@live.com>
2021-04-29 18:30:43 +0800

    feat: 🆕 Add Dialog component

---
+++ src/Component/BlazorComponent/Components/Dialog/BDialog.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public partial class BDialog : BDomComponentBase
+    {
+        protected CssBuilder BodyCssBuilder = new();
+        protected StyleBuilder BodyStyleBuilder = new();
+
+        [Parameter] public bool Visible { get; set; }
+
+        [Parameter] public EventCallback<bool> VisibleChanged { get; set; }
+
+        [Parameter] public StringOrNumber Width { get; set; }
+
+        [Parameter] public StringOrNumber MaxWidth { get; set; }
+
+        [Parameter] public bool Persistent { get; set; }
+        
+        [Parameter] public  bool Scrollable { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
