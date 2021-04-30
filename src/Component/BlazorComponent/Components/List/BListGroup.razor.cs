770ad999f2ff1d047f817a45b0a377647ca9c2b9
wangzengxing <71120280+wangzengxing@users.noreply.github.com>
2021-04-30 17:53:53 +0800

    feat:完善导航 (#33)
    * feat:完成ListGroup
    
    * refactor:调整group点击事件

---
+++ src/Component/BlazorComponent/Components/List/BListGroup.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public partial class BListGroup : BDomComponentBase
+    {
+        private const string PREPEND = "prepend";
+        private const string APPEND = "append";
+
+        [Parameter]
+        public string PrependIcon { get; set; }
+
+        [Parameter]
+        public string AppendIcon { get; set; }
+
+        [Parameter]
+        public RenderFragment Activator { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected bool IsActive { get; set; }
+    }
+}
