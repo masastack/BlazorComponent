1e8da1fab95b03c157acc511151c05e5ec8ad9ed
wangzengxing <71120280+wangzengxing@users.noreply.github.com>
2021-05-06 18:06:42 +0800

    feat:完成list的其他组件 (#34)

---
+++ src/Component/BlazorComponent/Components/List/BListItemActionText.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public partial class BListItemActionText : BDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
