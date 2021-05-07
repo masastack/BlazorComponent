0e7d11b97cb04bf448f99b61262994a2ff469d7c
caoyouxian <capdiem@live.com>
2021-05-07 17:50:43 +0800

    refactor(card): ♻ Rewrite Card component for supporting any childcontent

---
+++ src/Component/BlazorComponent/Components/Card/BCardTitle.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public partial class BCardTitle : BDomComponentBase
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
