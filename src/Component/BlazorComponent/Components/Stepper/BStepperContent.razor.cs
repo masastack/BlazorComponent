cc9592c8980f06fbae6ce58ffa4662cf6ab298c7
wangzengxing <1625322170@qq.com>
2021-06-01 14:51:05 +0800

    feat:初步完成步骤条

---
+++ src/Component/BlazorComponent/Components/Stepper/BStepperContent.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public partial class BStepperContent : BDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
