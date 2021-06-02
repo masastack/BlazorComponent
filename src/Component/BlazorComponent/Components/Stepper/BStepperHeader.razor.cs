860b54c142e9c4b57ba663d0db78c7220e15ab54
wangzengxing <1625322170@qq.com>
2021-06-02 13:23:57 +0800

    feat:完成步骤条

--- src/Component/BlazorComponent/Components/Stepper/BStepperHeader.razor.cs
+++ src/Component/BlazorComponent/Components/Stepper/BStepperHeader.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace BlazorComponent
 {
     public partial class BStepperHeader : BDomComponentBase
     {
-
+        [Parameter]
+        public RenderFragment ChildContent{ get; set; }
     }
 }
