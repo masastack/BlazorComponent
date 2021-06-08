ee2db1dc32cf8fadaa7dd1080b02b28552494934
1252147688@qq.com <1252147688@qq.com>
2021-06-08 10:53:28 +0800

    feat:Add timeline

---
+++ src/Component/BlazorComponent/Components/Timeline/BTimeline.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public abstract partial class BTimeline : BDomComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool Reverse { get; set; }
+    }
+}
---
+++ src/Component/BlazorComponent/Components/Timeline/BTimelineItem.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public partial class BTimelineItem : BDomComponentBase
+    {
+        [CascadingParameter]
+        protected BTimeline BTimeline { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public RenderFragment OppositeContent { get; set; }
+    }
+}
