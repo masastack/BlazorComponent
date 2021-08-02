cfd22075c97b592554f647b94bd48125008fdf00
王增星 <wangzengxing@masastack.com>
2021-08-02 05:24:08 +0000

    feat:form支持验证可枚举的属性

---
+++ src/Component/BlazorComponent/Components/Form/EnumerableValidationAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EnumerableValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var result = new EnumerableValidationResult();
+            if (value is IEnumerable enumerable)
+            {
+                foreach (var item in enumerable)
+                {
+                    var context = new ValidationContext(item);
+                    var validationResults = new List<ValidationResult>();
+                    Validator.TryValidateObject(item, context, validationResults, true);
+
+                    var descriptor = new ValidationResultDescriptor(item, validationResults);
+                    result.Descriptors.Add(descriptor);
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ src/Component/BlazorComponent/Components/Form/EnumerableValidationResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public class EnumerableValidationResult : ValidationResult
+    {
+        public EnumerableValidationResult()
+            : base("Invalid enumerable.")
+        {
+
+        }
+
+        public List<ValidationResultDescriptor> Descriptors { get; } = new();
+    }
+
+    public class ValidationResultDescriptor
+    {
+        public ValidationResultDescriptor(object objectInstance, List<ValidationResult> results)
+        {
+            ObjectInstance = objectInstance;
+            Results = results ?? throw new ArgumentNullException(nameof(results));
+        }
+
+        public object ObjectInstance { get; }
+
+        public List<ValidationResult> Results { get; }
+    }
+}
