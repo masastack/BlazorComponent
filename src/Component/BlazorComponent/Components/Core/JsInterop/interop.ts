7e9ff7b78d169400dbb05c97c2d8b14b499f381a
mayue <mayue@masastack.com>
2021-08-13 13:18:05 +0800

    appbar封装 《待重构》

--- src/Component/BlazorComponent/Components/Core/JsInterop/interop.ts
+++ src/Component/BlazorComponent/Components/Core/JsInterop/interop.ts
@@ -226,7 +226,12 @@ export function addDomEventListener(element, eventName, preventDefault, invoker)
         }
     } else {
         let dom = getDom(element);
-        (dom as HTMLElement).addEventListener(eventName, callback);
+        var htmlElement = dom as HTMLElement;
+        if(eventName == 'scroll'){
+            htmlElement.addEventListener(eventName, debounce(() => callback(getDomInfo(dom)), 200, false));
+        }else{
+            htmlElement.addEventListener(eventName, callback);
+        }
     }
 }
 
---
+++ src/Component/BlazorComponent/Components/Mixins/IScrollTarget.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorComponent
+{
+    public interface IScrollTarget
+    {
+        public string ScrollTarget { get; set; }
+        
+        public int ScrollThreshold { get; set; }
+    }
+}
