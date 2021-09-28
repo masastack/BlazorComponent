81e2f1736a023d94c326acc62a056d3d2125a78c
徐云升 <xuyunsheng@masastack.com>
2021-09-28 05:08:02 +0000

    Feat:systembar

---
+++ src/Component/BlazorComponent/Components/SystemBar/BSystemBar.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public partial class BSystemBar : BDomComponentBase, IHasProviderComponent
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        protected virtual async Task HandleOnClickAsync(MouseEventArgs args)
+        {
+            if (OnClick.HasDelegate)
+            {
+                await OnClick.InvokeAsync();
+            }
+        }
+
+        protected override void SetComponentClass()
+        {
+            AbstractProvider
+                .Apply(typeof(CascadingValue<>), typeof(CascadingValue<BSystemBar>), props =>
+                {
+                    props[nameof(CascadingValue<BSystemBar>.Value)] = this;
+                });
+        }
+    }
+}
