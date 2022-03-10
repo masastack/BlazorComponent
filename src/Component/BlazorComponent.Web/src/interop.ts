9c9d8af4a3fc7cdcfb78c13c101d3e7a7b7c74c2
capdiem <capdiem@live.com>
2022-03-10 19:17:45 +0800

    🆕 feat(copyable-text): Add copyChild and copyText functions

--- src/Component/BlazorComponent.Web/src/interop.ts
+++ src/Component/BlazorComponent.Web/src/interop.ts
@@ -1250,3 +1250,51 @@ export function containsActiveElement(selector) {
 
     return null;
 }
+
+export function copyChild (el) {
+    if (typeof el === 'string') {
+        el = document.querySelector(el);
+    }
+
+    if (!el) return;
+
+    el.setAttribute('contenteditable', 'true');
+    el.focus();
+    document.execCommand('selectAll', false, null);
+    document.execCommand('copy');
+    document.execCommand('unselect');
+    el.removeAttribute('contenteditable');
+}
+
+export function copyText(text) {
+    if (!navigator.clipboard) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Fallback: Copying text command was ' + msg);
+        } catch (err) {
+            console.error('Fallback: Oops, unable to copy', err);
+        }
+
+        document.body.removeChild(textArea);
+        return;
+    }
+
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
