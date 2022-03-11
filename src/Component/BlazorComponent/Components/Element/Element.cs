24381266dcbf9f7cb8089e3ab5b301804c60050a
wangzengxing <1625322170@qq.com>
2022-03-11 10:33:12 +0800

    refactor(element):refactor element

--- src/Component/BlazorComponent/Components/Element/Element.cs
+++ src/Component/BlazorComponent/Components/Element/Element.cs
@@ -1,32 +1,21 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
+﻿using Microsoft.AspNetCore.Components.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorComponent
 {
-    public class Element : BComponentBase
+    public class Element : ComponentBase
     {
-        private bool _firstElement;
-
-        public Element()
-        {
-            Watcher = new(GetType());
-        }
-
         [Parameter]
         public string Tag { get; set; } = "div";
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
-
-        //TODO: remove this
-        [Parameter]
-        public Action<ElementReference> ReferenceCaptureAction { get; set; }
 
         [Parameter]
         public string Class { get; set; }
@@ -35,169 +24,33 @@ namespace BlazorComponent
         public string Style { get; set; }
 
         [Parameter]
-        public bool? Show
-        {
-            get
-            {
-                return GetValue<bool?>();
-            }
-            set
-            {
-                SetValue(value);
-            }
-        }
-
-        [Parameter]
-        public bool? If
-        {
-            get
-            {
-                return GetValue<bool?>();
-            }
-            set
-            {
-                SetValue(value);
-            }
-        }
-
-        [Parameter]
-        public object Key
-        {
-            get
-            {
-                return GetValue<object>();
-            }
-            set
-            {
-                SetValue(value);
-            }
-        }
-
-        [CascadingParameter]
-        public Transition Transition { get; set; }
+        public Action<ElementReference> ReferenceCaptureAction { get; set; }
 
         [Parameter(CaptureUnmatchedValues = true)]
-        public IDictionary<string, object> ExtraAttributes { get; set; }
+        public virtual IDictionary<string, object> AdditionalAttributes { get; set; }
+
+        protected virtual string ComputedClass => Class;
+
+        protected virtual string ComputedStyle => Style;
 
         public ElementReference Reference { get; protected set; }
 
-        protected Action<ElementReference> ComputedReferenceCaptureAction
-        {
-            get
-            {
-                if (ReferenceCaptureAction == null)
-                {
-                    return reference => Reference = reference;
-                }
-
-                return reference =>
-                {
-                    ReferenceCaptureAction(reference);
-                    Reference = reference;
-                };
-            }
-        }
-
-        protected CssBuilder CssBuilder { get; } = new();
-
-        protected StyleBuilder StyleBuilder { get; } = new();
-
-        protected PropertyWatcher Watcher { get; }
-
-        protected TValue GetValue<TValue>(TValue @default = default, [CallerMemberName] string name = null)
-        {
-            return Watcher.GetValue(@default, name);
-        }
-
-        protected void SetValue<TValue>(TValue value, [CallerMemberName] string name = null)
-        {
-            Watcher.SetValue(value, name);
-        }
-
-        public virtual async Task UpdateViewAsync()
-        {
-            await InvokeStateHasChangedAsync();
-        }
-
-        protected bool? InternalIf { get; set; } = true;
-
-        protected override void OnInitialized()
-        {
-            CssBuilder
-                .Add(() => Class)
-                .AddIf(() => Transition.Class, () => Transition != null && _firstElement);
-
-            StyleBuilder
-                .Add(() => Style)
-                .AddIf(() => Transition.Style, () => Transition != null && _firstElement)
-                .AddIf("display:none", () => Transition == null && Show == false);
-
-            if (Transition != null)
-            {
-                _firstElement = Transition.Register(this);
-                if (_firstElement)
-                {
-                    if (If != null && Key != null)
-                    {
-                        InternalIf = If;
-                    }
-
-                    Watcher
-                        .Watch<bool?>(nameof(Show), val =>
-                        {
-                            NextTick(() =>
-                            {
-                                //Dom may not ready,so we move transition to nextTick
-                                Transition.RunTransition(TransitionMode.Show, val.Value);
-                                return Task.CompletedTask;
-                            });
-                        })
-                        .Watch<bool?>(nameof(If), val =>
-                        {
-                            NextTick(() =>
-                            {
-                                Transition.RunTransition(TransitionMode.If, val.Value);
-                                return Task.CompletedTask;
-                            });
-                        })
-                        .Watch<object>(nameof(Key), () =>
-                        {
-                            NextTick(() =>
-                            {
-                                InternalIf = !InternalIf;
-                                Transition.RunTransition(TransitionMode.If, InternalIf.Value);
-
-                                return Task.CompletedTask;
-                            });
-                        });
-                }
-            }
-        }
-
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
-            if (Transition == null || Transition.If != false)
+            var sequence = 0;
+            builder.OpenElement(sequence++, Tag);
+
+            builder.AddMultipleAttributes(sequence++, AdditionalAttributes);
+            builder.AddAttribute(sequence++, "class", ComputedClass);
+            builder.AddAttribute(sequence++, "style", ComputedStyle);
+            builder.AddContent(sequence++, ChildContent);
+            builder.AddElementReferenceCapture(sequence++, reference =>
             {
-                var sequence = 0;
-                builder.OpenElement(sequence++, Tag);
+                ReferenceCaptureAction?.Invoke(reference);
+                Reference = reference;
+            });
 
-                builder.AddAttribute(sequence++, "class", CssBuilder.Class);
-                builder.AddAttribute(sequence++, "style", StyleBuilder.Style);
-
-                builder.AddMultipleAttributes(sequence++, ExtraAttributes);
-
-                builder.AddContent(sequence++, wrapperBuilder =>
-                {
-                    wrapperBuilder.OpenComponent<ElementWrapper>(0);
-                    wrapperBuilder.AddAttribute(1, nameof(ElementWrapper.Value), Transition == null || Transition.State == TransitionState.None);//When we play animation,don't update ChildContent
-                    wrapperBuilder.AddAttribute(2, nameof(ChildContent), ChildContent);
-                    wrapperBuilder.CloseComponent();
-                });
-
-                builder.AddElementReferenceCapture(sequence++, ComputedReferenceCaptureAction);
-
-                builder.CloseElement();
-            }
+            builder.CloseComponent();
         }
     }
 }
