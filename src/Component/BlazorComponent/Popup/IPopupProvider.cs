844756c2bcbe20c1c89d138f6aca240d20f34d20
capdiem <capdiem@live.com>
2022-03-15 16:14:15 +0800

    🆕 feat(popup): Support rendering dynamic component from PopupProvider

---
+++ src/Component/BlazorComponent/Popup/IPopupProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorComponent;
+
+public interface IPopupProvider
+{
+    event EventHandler StateChanged;
+    
+    ProviderItem Add(Type componentType, Dictionary<string, object> attributes, object service, string serviceName);
+    void Remove(ProviderItem item);
+    IEnumerable<ProviderItem> GetItems();
+}
---
+++ src/Component/BlazorComponent/Popup/ProviderItem.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorComponent;
+
+public class ProviderItem
+{
+    public Type ComponentType { get; set; }
+
+    public PopupProvider Provider { get; set; }
+    
+    public TaskCompletionSource<object> TaskCompletionSource { get; set; }
+
+    public object Service { get; set; }
+
+    public string ServiceName { get; set; }
+
+    public Dictionary<string, object> Parameters { get; set; }
+
+    public ProviderItem()
+    {
+        TaskCompletionSource = new();
+        Parameters = new();
+    }
+
+    public void Discard(object result)
+    {
+        TaskCompletionSource.TrySetResult(result);
+        Provider.Remove(this);
+    }
+}
