f2c9ab42856451cd46d44173fbec8d6c72c2d97b
wangzengxing <1625322170@qq.com>
2022-03-16 13:32:59 +0800

    refactor(menuable):refactor menuable

---
+++ src/Component/BlazorComponent/Mixins/Menuable/MultipleResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorComponent
+{
+    public class MultipleResult
+    {
+        public WindowAndDocument WindowAndDocument { get; set; }
+
+        public MenuableDimensions Dimensions { get; set; }
+
+        public int ZIndex { get; set; }
+    }
+}
