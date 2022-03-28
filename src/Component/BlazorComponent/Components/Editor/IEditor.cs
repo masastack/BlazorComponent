9449fc9e80cf14dda1c8684c21717c7206c5f1ea
wzh425 <wuzihao425@outlook.com>
2022-03-28 19:16:19 +0800

    feat:add new Editor Component (#81)
    * feat:add new Editor Component
    
    * modify some naming conventions
    
    * 移除EditorContent插槽
    
    * 增加换行

---
+++ src/Component/BlazorComponent/Components/Editor/IEditor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponent
+{
+    public interface IEditor : IHasProviderComponent
+    {
+        RenderFragment ToolbarContent { get; }
+        string Placeholder { get; }
+        Task<string> GetTextAsync();
+        Task<string> GetHtmlAsync();
+        Task<string> GetContentAsync();
+        Task SetHtmlAsync();
+        Task SetContentAsync();
+    }
+}
