79cd7981d7779968a1848d6feaca2c5c7af0e2aa
capdiem <capdiem@live.com>
2022-04-24 11:34:23 +0800

    🐛 fix(transition): Components such as button render visible delays. (#108)

--- src/Component/BlazorComponent/Components/Transition/Elements/ShowTransitionElement.cs
+++ src/Component/BlazorComponent/Components/Transition/Elements/ShowTransitionElement.cs
@@ -1,18 +1,17 @@
-﻿namespace BlazorComponent
-{
-    public class ShowTransitionElement : ToggleableTransitionElement
-    {
-        protected override string ComputedStyle
-        {
-            get
-            {
-                if (!LazyValue)
-                {
-                    return string.Join(";", base.ComputedStyle, "display:none");
-                }
+﻿namespace BlazorComponent;
 
-                return base.ComputedStyle;
+public class ShowTransitionElement : ToggleableTransitionElement
+{
+    protected override string ComputedStyle
+    {
+        get
+        {
+            if (!LazyValue)
+            {
+                return string.Join(";", base.ComputedStyle, "display:none");
             }
+
+            return base.ComputedStyle;
         }
     }
 }
