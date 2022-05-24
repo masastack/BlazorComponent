1d7fa64b0da9d2a40d0ce17bf65227befb5aa352
qinyouzeng <qinyouzeng@masastack.com>
2022-05-24 18:29:37 +0800

    feat: sorttablejs add and test

--- src/Component/BlazorComponent/Components/Dragzone/BDragItem.cs
+++ src/Component/BlazorComponent/Components/Dragzone/BDragItem.cs
@@ -10,10 +10,6 @@
 
         public string CssStyle { get; set; } = string.Empty;
 
-        public double X { get; set; }
-
-        public double Y { get; set; }
-
         public T Item { get; set; }
     }
 }
