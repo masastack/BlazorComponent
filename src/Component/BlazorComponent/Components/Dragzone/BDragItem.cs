7d285068b1397884022f884cb42c96130b621aa0
qinyouzeng <qinyouzeng@masastack.com>
2022-05-25 19:53:51 +0800

    docs: doc add

--- src/Component/BlazorComponent/Components/Dragzone/BDragItem.cs
+++ src/Component/BlazorComponent/Components/Dragzone/BDragItem.cs
@@ -2,10 +2,6 @@
 {
     public class BDragItem<T>
     {
-        public bool Enable { get; set; } = true;
-
-        public bool Fixed { get; set; } = false;
-
         public string CssClass { get; set; } = string.Empty;
 
         public string CssStyle { get; set; } = string.Empty;
