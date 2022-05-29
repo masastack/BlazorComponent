5764f1b78a973f53de5ff845ef511ffc37f26beb
Qin <Qin@DESKTOP-JCUJA40>
2022-05-29 16:27:31 +0800

    add files to change filename uplowercase

---
+++ src/Component/BlazorComponent/Components/DragZone/BDragItem.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorComponent
+{
+    public partial class BDragItem : BDomComponentBase
+    {
+        protected override void OnInitialized()
+        {
+            DragZone.Register(this);
+            base.OnInitialized();
+        }
+
+        [CascadingParameter]
+        public BDragZone DragZone { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
