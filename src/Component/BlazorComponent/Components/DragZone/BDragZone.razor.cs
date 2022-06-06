7f9558e832b1541df6822ec6b0be7c3cb8ecf0d3
qinyouzeng <qinyouzeng@masastack.com>
2022-06-06 18:08:23 +0800

    feat: sort update

--- src/Component/BlazorComponent/Components/DragZone/BDragZone.razor.cs
+++ src/Component/BlazorComponent/Components/DragZone/BDragZone.razor.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorComponent
+﻿using Microsoft.Extensions.Logging;
+
+namespace BlazorComponent
 {
     public partial class BDragZone : BDomComponentBase, IThemeable
     {
@@ -13,6 +15,9 @@
 
         [Inject]
         public BDragDropService DragDropService { get; set; }
+
+        [Inject]
+        public ILogger<BDragZone> Logger { get; set; }
 
         [Parameter]
         public RenderFragment ChildContent { get; set; }
@@ -54,14 +59,14 @@
         public void Add(BDragItem item, int position = -1)
         {
             Add(Items, item, position);
-            Add(DynicItems, item);
+            //Add(DynicItems, item);
             SetIndex();
         }
 
         public void AddRange(IEnumerable<BDragItem> sources, int position = -1)
         {
             AddRange(sources, Items, position);
-            AddRange(sources, DynicItems, position);
+            //AddRange(sources, DynicItems, position);
             SetIndex();
         }
 
@@ -72,7 +77,7 @@
             foreach (var item in items)
             {
                 Remove(Items, item);
-                Remove(DynicItems, item);
+                //Remove(DynicItems, item);
             }
             SetIndex();
         }
@@ -93,27 +98,84 @@
 
         public bool Update(BDragItem item, int oldIndex, int newIndex)
         {
+
+            Logger.LogInformation($"update start ids : { string.Join(",", Items.Select(m => m.Id))}");
+            //var list = Items.ToArray().ToList();
+            ////Items.Clear();
+            //list[oldIndex] = null;
+            //list.RemoveAt(oldIndex);
+
+            //bool isAppend = false;
+            //if (newIndex - oldIndex > 0)
+            //{
+            //    newIndex--;
+            //    isAppend = true;
+            //}
+
+            //if (isAppend && list.Count - newIndex - 1 == 0)
+            //    list.Add(item);
+            //else
+            //    list.Insert(newIndex, item);
+
+            //Items = list;
+
+           
+            //var array = Items.ToArray();
+            //array[oldIndex] = null;
+            //array.r
+
+
+
+
+
+            //var array = Items.ToArray();
+            //var temp = array[newIndex];
+            //array[oldIndex] = null;
+
+            //int index = 0, count = array.Length;
+
+            //while (count - index > 0)
+            //{ 
+            //    if()
+            //}
+
+
+
+
+
+
             if (oldIndex - newIndex < 0)
             {
-
+                Items.RemoveAt(oldIndex);
                 if (newIndex - Items.Count == 0)
                 {
-                    Items.Append(item);
+                    Items = Items.Append(item).ToList();
                 }
                 else
                 {
-                    Items.Insert(newIndex + 1, item);
+                    Items.Insert(newIndex, item);
                 }
-                Items.RemoveAt(oldIndex);
             }
             else
             {
                 Items.RemoveAt(oldIndex);
                 Items.Insert(newIndex, item);
             }
-
+            Logger.LogInformation($"update end ids : { string.Join(",", Items.Select(m => m.Id))}");
             SetIndex();
             return true;
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            Logger.LogInformation($"OnAfterRenderAsync ids : { string.Join(",", Items.Select(m => m.Id))}");
+            return base.OnAfterRenderAsync(firstRender);
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            Logger.LogInformation($"OnAfterRender ids : { string.Join(",", Items.Select(m => m.Id))}");           
+            base.OnAfterRender(firstRender);
         }
 
         private void AddRange(IEnumerable<BDragItem> sources, List<BDragItem> target, int position = -1)
@@ -153,7 +215,7 @@
 
         private void SetIndex()
         {
-            StateHasChanged();
+            //Logger.LogInformation($"SetIndex ids : { string.Join(",", Items.Select(m => m.Id))}");
             //int index = 0, count = Items.Count;
             //while (count - index > 0)
             //{
@@ -161,6 +223,7 @@
             //        Items[index].Value = index;
             //    index++;
             //}
+            //StateHasChanged();
         }
     }
 }
