efb1db1cb8756430d8a9ecfd4f14db88856dc3f4
capdiem <capdiem@live.com>
2022-06-08 10:33:47 +0800

    ♻ refactor(DataFooter): split IDataFooter (#145)

--- src/Component/BlazorComponent/Components/DataIterator/DataFooter/IDataFooter.cs
+++ src/Component/BlazorComponent/Components/DataIterator/DataFooter/IDataFooter.cs
@@ -1,44 +1,20 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace BlazorComponent
+﻿namespace BlazorComponent
 {
-    public interface IDataFooter : IHasProviderComponent
+    public interface IDataFooter : IDataFooterParameters, IHasProviderComponent
     {
         IEnumerable<DataItemsPerPageOption> ComputedDataItemsPerPageOptions { get; }
-
-        string ItemsPerPageText { get; }
-
-        DataPagination Pagination { get; }
 
         RenderFragment<(int PageStart, int PageStop, int ItemsLength)> PageTextContent { get; }
 
         Task HandleOnPreviousPageAsync();
 
-        string PageText { get; }
-
-        bool ShowCurrentPage { get; }
-
-        DataOptions Options { get; }
-
-        bool DisablePagination { get; }
-
         Task HandleOnNextPageAsync();
 
         bool RTL { get; }
 
-        string NextIcon { get; }
-
-        string PrevIcon { get; }
-
         Task HandleOnFirstPageAsync();
 
         bool DisableNextPageIcon { get; }
-
-        bool ShowFirstLastPage { get; }
-
-        string LastIcon { get; }
-
-        string FirstIcon { get; }
 
         Task HandleOnLastPageAsync();
     }
