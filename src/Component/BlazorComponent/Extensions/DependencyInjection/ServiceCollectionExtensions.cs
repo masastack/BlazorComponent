7b28299db3bfb8b85e52e30770832d80f86152fc
wuweilai <824255785@qq.com>
2022-06-13 17:54:15 +0800

    feature(form):fluentValidation-DependencyInjection

--- src/Component/BlazorComponent/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Component/BlazorComponent/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using BlazorComponent;
 using BlazorComponent.Web;
-using Microsoft.AspNetCore.Components;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Globalization;
 
@@ -18,7 +18,31 @@ namespace Microsoft.Extensions.DependencyInjection
             services.TryAddSingleton<IComponentIdGenerator, GuidComponentIdGenerator>();
             CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.CurrentCulture;
             services.AddSingleton<IComponentActivator, AbstractComponentActivator>();
+            services.AddFluentValidation();
 
+            return services;
+        }
+
+        private static IServiceCollection AddFluentValidation(this IServiceCollection services)
+        {
+            try
+            {
+                var referenceAssemblys = AppDomain.CurrentDomain.GetAssemblies();
+                foreach (var referenceAssembly in referenceAssemblys)
+                {
+                    if (referenceAssembly.FullName.StartsWith("Microsoft.") || referenceAssembly.FullName.StartsWith("System."))
+                        continue;
+
+                    var types = referenceAssembly.GetTypes().Where(t => t.BaseType?.IsGenericType == true && t.BaseType.GetGenericTypeDefinition() == typeof(AbstractValidator<>)).ToArray();
+                    foreach (var type in types)
+                    {
+                        services.AddTransient(type);
+                    }
+                }
+            }
+            catch
+            {
+            }
             return services;
         }
     }
