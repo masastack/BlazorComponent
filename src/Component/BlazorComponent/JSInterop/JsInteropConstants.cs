78e0b69d72a58dbeb1f5fcc87491d37574e48dce
capdiem <capdiem@live.com>
2022-06-20 16:30:37 +0800

    🆕 feat(js-interop): add enablePreventDefaultForEvent (#153)
    * 🆕 feat(js-interop): add enablePreventDefaultForEvent
    
    * 🐛 fix(BSelect): chip selection canno be focused

--- src/Component/BlazorComponent/JSInterop/JsInteropConstants.cs
+++ src/Component/BlazorComponent/JSInterop/JsInteropConstants.cs
@@ -134,7 +134,7 @@
 
         public static string GetImageDimensions => $"{Variables.JsInteropFuncNamePrefix}getImageDimensions";
 
-        public static string PreventDefaultOnArrowUpDown => $"{Variables.JsInteropFuncNamePrefix}preventDefaultOnArrowUpDown";
+        public static string EnablePreventDefaultForEvent => $"{Variables.JsInteropFuncNamePrefix}enablePreventDefaultForEvent";
 
         public static string Observer => $"{Variables.JsInteropFuncNamePrefix}observer";
 
