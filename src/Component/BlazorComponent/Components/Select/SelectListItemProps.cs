5d5a115881f844e1af67879b2531881d2abd8d01
capdiem <capdiem@live.com>
2022-08-17 15:02:32 +0800

    🆕 feat(Select): add OnClick to ItemContent's context

--- src/Component/BlazorComponent/Components/Select/SelectListItemProps.cs
+++ src/Component/BlazorComponent/Components/Select/SelectListItemProps.cs
@@ -1,12 +1,14 @@
-﻿namespace BlazorComponent
-{
-    public class SelectListItemProps<TItem>
-    {
-        public SelectListItemProps(TItem item)
-        {
-            Item = item;
-        }
+﻿namespace BlazorComponent;
 
-        public TItem Item { get; }
+public class SelectListItemProps<TItem>
+{
+    public SelectListItemProps(TItem item, Func<Task> onClick)
+    {
+        Item = item;
+        OnClick = onClick;
     }
+
+    public TItem Item { get; }
+
+    public Func<Task> OnClick { get; }
 }
