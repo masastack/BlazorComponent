2e93900f7194de3d32a2e6a6bd11cd7b2564dc08
capdiem <capdiem@live.com>
2023-01-17 17:37:06 +0800

    🆕 feat(I18n): Add I18n component

---
+++ src/Component/BlazorComponent/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorComponent;
+
+public static class StringExtensions
+{
+    public static string ReplaceFirst(this string str, string oldValue, string newValue)
+    {
+        if (string.IsNullOrEmpty(oldValue))
+        {
+            return str;
+        }
+
+        var index = str.IndexOf(oldValue, StringComparison.Ordinal);
+        if (index == -1)
+        {
+            return str;
+        }
+
+        // todo: regex.replace is more fast?
+
+        str = str.Remove(index, oldValue.Length);
+        return str.Insert(index, newValue);
+    }
+}
