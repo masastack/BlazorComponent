e5567c53e69872d1845c5df6302c327f7a640ace
Ximer <1178890320@qq.com>
2023-01-25 16:43:39 +0800

    Remove injection codes

--- src/Component/BlazorComponent.Web/src/proxies/maps/baidumap/index.js
+++ src/Component/BlazorComponent.Web/src/proxies/maps/baidumap/index.js
@@ -1,28 +1,6 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-async function injectBaiduMapScript(ak) {
-    if (!document.getElementById(ak)) {
-        var script = document.createElement("script");
-        script.src = "https://api.map.baidu.com/getscript?type=webgl&v=1.0&ak=" + ak;
-        script.type = "text/javascript";
-        script.id = ak;
-        document["head"].append(script);
-    }
-
-    if (!document.getElementById("baidu-map-css")) {
-        var css = document.createElement("link");
-        css.href = "https://api.map.baidu.com/res/webgl/10/bmap.css";
-        css.type = "text/css";
-        css.rel = "stylesheet";
-        css.id = "baidu-map-css";
-        document["head"].append(css);
-    }
-
-    window.BMAP_PROTOCOL = "https";
-    window.BMapGL_loadScriptTime = (new Date).getTime();
-}
-
-async function loadMap(containerId, initArgs) {
+async function initMap(containerId, initArgs) {
     try {
         var map = new BMapGL.Map(containerId);
 
@@ -30,14 +8,20 @@ async function loadMap(containerId, initArgs) {
             map.enableScrollWheelZoom();
         else
             map.disableScrollWheelZoom();
-        
+
         map.centerAndZoom(new BMapGL.Point(initArgs.center.x, initArgs.center.y), initArgs.zoom);
+
+        if (initArgs.dark)
+            map.setMapStyleV2({
+                styleId: initArgs.darkThemeId
+            });
 
         return map;
     } catch (error) {
+        console.log(error);
         await delay(100);
-        return await loadMap(containerId, initArgs);
+        return await initMap(containerId, initArgs);
     }
 }
 
-export { injectBaiduMapScript, loadMap }
+export { initMap }
