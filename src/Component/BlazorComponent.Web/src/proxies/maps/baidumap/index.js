4ecbe71a786ae9c0eb7261d6aac72c022e770eda
Ximer <1178890320@qq.com>
2023-01-25 13:29:01 +0800

    Optimize script injection logic

--- src/Component/BlazorComponent.Web/src/proxies/maps/baidumap/index.js
+++ src/Component/BlazorComponent.Web/src/proxies/maps/baidumap/index.js
@@ -1,30 +1,38 @@
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 async function injectBaiduMapScript(ak) {
-    // if script has been loaded, return. 
-    if (document.getElementById("baidumap-script"))
-        return;
+    if (!document.getElementById(ak)) {
+        var script = document.createElement("script");
+        script.src = "https://api.map.baidu.com/getscript?type=webgl&v=1.0&ak=" + ak;
+        script.type = "text/javascript";
+        script.id = ak;
+        document["head"].append(script);
+    }
 
-    var script = document.createElement("script");
-    script.src = "https://api.map.baidu.com/getscript?type=webgl&v=1.0&ak=" + ak;
-    script.type = "text/javascript";
-    script.id = "baidumap-script";
-
-    var css = document.createElement("link");
-    css.href = "https://api.map.baidu.com/res/webgl/10/bmap.css";
-    css.type = "text/css";
-    css.rel = "stylesheet";
+    if (!document.getElementById("baidu-map-css")) {
+        var css = document.createElement("link");
+        css.href = "https://api.map.baidu.com/res/webgl/10/bmap.css";
+        css.type = "text/css";
+        css.rel = "stylesheet";
+        css.id = "baidu-map-css";
+        document["head"].append(css);
+    }
 
     window.BMAP_PROTOCOL = "https";
     window.BMapGL_loadScriptTime = (new Date).getTime();
-    document["head"].append(script, css);
 }
 
 async function loadMap(containerId, initArgs) {
     try {
         var map = new BMapGL.Map(containerId);
-        map.enableScrollWheelZoom(initArgs.canZoom);
+
+        if (initArgs.enableScrollWheelZoom)
+            map.enableScrollWheelZoom();
+        else
+            map.disableScrollWheelZoom();
+        
         map.centerAndZoom(new BMapGL.Point(initArgs.center.x, initArgs.center.y), initArgs.zoom);
+
         return map;
     } catch (error) {
         await delay(100);
