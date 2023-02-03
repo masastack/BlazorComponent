13f5395ecd72a9e3344a555693fedf88e08be932
沙雕の方块 <53342258+LittleFish-233@users.noreply.github.com>
2023-02-03 12:11:31 +0800

    feat: Support for dynamic theme modification (#299)
    * feat: Support for dynamic theme modification
    
    * Modify the property name

--- src/Component/BlazorComponent/Components/App/BApp.razor.cs
+++ src/Component/BlazorComponent/Components/App/BApp.razor.cs
@@ -19,6 +19,8 @@ namespace BlazorComponent
         [CascadingParameter(Name = "IsDark")]
         public bool CascadingIsDark { get; set; }
 
+        protected string ThemeStyleMarkups { get; set; } = "";
+
         protected override void OnInitialized()
         {
             PopupProvider.StateChanged += OnStateChanged;
