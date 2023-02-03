d1b39cb8b727b8b31379ec19651fda52577106ce
capdiem <capdiem@live.com>
2023-02-03 14:56:10 +0800

    ♻ refactor: simplify code

--- src/Component/BlazorComponent.Web/src/proxies/echarts/index.ts
+++ src/Component/BlazorComponent.Web/src/proxies/echarts/index.ts
@@ -2,10 +2,7 @@ function init(elOrString, theme, initOptions) {
   const chart = echarts.init(elOrString, theme, initOptions);
 
   window.addEventListener("resize", () => {
-    const dom = chart.getDom();
-    const width = dom.clientWidth;
-    const height = dom.clientHeight;
-    chart.resize({ width, height });
+    chart.resize();
   });
 
   const observer = new IntersectionObserver((entries) => {
@@ -41,16 +38,4 @@ function setOption(
   }
 }
 
-function dispose(instance: echarts.ECharts) {
-  if (instance) {
-    instance.dispose();
-  }
-}
-
-function resize(instance: echarts.ECharts, width?: number, height?: number) {
-  if (instance) {
-    instance.resize({ width, height });
-  }
-}
-
-export { init, setOption, resize, dispose };
+export { init, setOption };
