dca4b75c1431c141759edd4b91bda1a7df13ee0a
capdiem <capdiem@live.com>
2023-02-10 14:48:37 +0800

    ♻ refactor(ECharts): refactor the proxy using class

--- src/Component/BlazorComponent.Web/src/proxies/echarts/index.ts
+++ src/Component/BlazorComponent.Web/src/proxies/echarts/index.ts
@@ -1,41 +1,52 @@
+import { ECharts, EChartsResizeOption } from "echarts";
+
+class EChartsProxy {
+  instance: ECharts;
+  observer: IntersectionObserver;
+
+  constructor(
+    elOrString: HTMLDivElement | HTMLCanvasElement,
+    theme?: string,
+    initOptions?: any
+  ) {
+    this.instance = echarts.init(elOrString, theme, initOptions);
+
+    window.addEventListener("resize", () => {
+      this.instance.resize();
+    });
+
+    this.observer = new IntersectionObserver((entries) => {
+      if (entries.some((e) => e.isIntersecting)) {
+        this.instance.resize();
+      }
+    });
+
+    this.observer.observe(this.instance.getDom());
+  }
+
+  setOption(
+    option: any,
+    notMerge: boolean = false,
+    lazyUpdate: boolean = false
+  ) {
+    this.instance.setOption(option, notMerge, lazyUpdate);
+  }
+
+  resize(opts?: EChartsResizeOption) {
+    this.instance.resize(opts);
+  }
+
+  dispose() {
+    if (this.instance.isDisposed()) return;
+
+    this.observer.disconnect();
+
+    this.instance.dispose();
+  }
+}
+
 function init(elOrString, theme, initOptions) {
-  const chart = echarts.init(elOrString, theme, initOptions);
-
-  window.addEventListener("resize", () => {
-    chart.resize();
-  });
-
-  const observer = new IntersectionObserver((entries) => {
-    if (entries.some((e) => e.isIntersecting)) {
-      chart.resize();
-    }
-  });
-
-  observer.observe(chart.getDom());
-
-  if (!!chart["origin_dispose"]) {
-    chart["origin_dispose"] = chart.dispose;
-    chart.dispose = () => {
-      if (chart.isDisposed()) return;
-
-      observer.disconnect();
-
-      chart["origin_dispose"] && chart["origin_dispose"]();
-    };
-  }
-
-  return chart;
+  return new EChartsProxy(elOrString, theme, initOptions);
 }
 
-function setOption(
-  instance: echarts.ECharts,
-  option,
-  notMerge: boolean = false,
-  lazyUpdate: boolean = false
-) {
-  if (instance) {
-    instance.setOption(option, notMerge, lazyUpdate);
-  }
-}
-
-export { init, setOption };
+export { init };
