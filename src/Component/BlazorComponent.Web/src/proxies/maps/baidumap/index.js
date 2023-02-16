17422075bf1e46153cf72e861ea5e0fc5aea02f9
Ximer <1178890320@qq.com>
2023-02-16 11:50:52 +0800

    Improve code indent

--- src/Component/BlazorComponent.Web/src/proxies/maps/baidumap/index.js
+++ src/Component/BlazorComponent.Web/src/proxies/maps/baidumap/index.js
@@ -1,168 +1,167 @@
 class BaiduMapProxy {
-    instance;
-    dotNetHelper;
+  instance;
+  dotNetHelper;
 
-    constructor(containerId, initArgs) {
-        this.instance = new BMapGL.Map(containerId);
+  constructor(containerId, initArgs) {
+    this.instance = new BMapGL.Map(containerId);
 
-        if (initArgs.enableScrollWheelZoom)
-            this.instance.enableScrollWheelZoom();
+    if (initArgs.enableScrollWheelZoom)
+      this.instance.enableScrollWheelZoom();
 
-        this.instance.centerAndZoom(initArgs.center, initArgs.zoom);
+      this.instance.centerAndZoom(initArgs.center, initArgs.zoom);
 
-        if (initArgs.dark)
-            this.instance.setMapStyleV2({
-                styleId: initArgs.darkThemeId
-            });
-    }
-
-    setDotNetObjectReference(dotNetHelper, events) {
-        this.dotNetHelper = dotNetHelper;
-
-        events.forEach((event_name) => {
-            this.instance.addEventListener(event_name, async function (e) {
-                if (event_name == "dragstart" ||
-                    event_name == "dragging" ||
-                    event_name == "dragend" ||
-                    event_name == "dblclick") {
-
-                    await dotNetHelper.invokeMethodAsync("OnEvent", event_name, {
-                        latlng: e.point,
-                        pixel: e.pixel,
-                    });
-                }
-
-                else if (event_name == "click" ||
-                    event_name == "rightclick" ||
-                    event_name == "rightdblclick" ||
-                    event_name == "mousemove") {
-
-                    await dotNetHelper.invokeMethodAsync("OnEvent", event_name, {
-                        latlng: e.latlng,
-                        pixel: e.pixel,
-                    });
-                }
-
-                else
-                    await dotNetHelper.invokeMethodAsync("OnEvent", event_name, null);
-            });
+      if (initArgs.dark)
+        this.instance.setMapStyleV2({
+          styleId: initArgs.darkThemeId
         });
-    }
+  }
 
-    getOriginInstance = () => this.instance;
+  setDotNetObjectReference(dotNetHelper, events) {
+      this.dotNetHelper = dotNetHelper;
 
-    getCenter = () => this.instance.getCenter();
+      events.forEach((event_name) => {
+        this.instance.addEventListener(event_name, async function (e) {
+          if (event_name == "dragstart" ||
+              event_name == "dragging" ||
+              event_name == "dragend" ||
+              event_name == "dblclick") {
+                await dotNetHelper.invokeMethodAsync("OnEvent", event_name, {
+                  latlng: e.point,
+                  pixel: e.pixel,
+                });
+            }
 
-    setZoom = (zoom) => this.instance.setZoom(zoom);
+            else if (event_name == "click" ||
+              event_name == "rightclick" ||
+              event_name == "rightdblclick" ||
+              event_name == "mousemove") {
+                await dotNetHelper.invokeMethodAsync("OnEvent", event_name, {
+                  latlng: e.latlng,
+                  pixel: e.pixel,
+                });
+            }
 
-    getZoom = () => this.instance.getZoom();
-
-    setMaxZoom = (maxZoom) => this.instance.setMaxZoom(maxZoom);
-
-    setMinZoom = (minZoom) => this.instance.setMinZoom(minZoom);
-
-    enableScrollWheelZoom = () => this.instance.enableScrollWheelZoom();
-
-    disableScrollWheelZoom = () => this.instance.disableScrollWheelZoom();
-
-    setMapType = (mapType) => this.instance.setMapType(mapType);
-
-    getMapType = () => this.instance.getMapType();
-
-    setTrafficOn = () => this.instance.setTrafficOn();
-
-    setTrafficOff = () => this.instance.setTrafficOff();
-
-    setMapStyleV2 = (options) => this.instance.setMapStyleV2(options);
-
-    panTo = (point) => this.instance.panTo(point);
-
-    addOverlay = (overlay) => this.instance.addOverlay(overlay);
-
-    removeOverlay = (overlay) => this.instance.removeOverlay(overlay);
-
-    clearOverlays = () => this.instance.clearOverlays();
-
-    initAndAddCircle(circle) {
-        var c = new BMapGL.Circle(circle.center, circle.radius, {
-            strokeColor: circle.strokeColor,
-            strokeWeight: circle.strokeWeight,
-            strokeOpacity: circle.strokeOpacity,
-            strokeStyle: circle.strokeStyle == 0 ? "solid" : "dashed",
-            fillColor: circle.fillColor,
-            fillOpacity: circle.fillOpacity
+            else {
+              await dotNetHelper.invokeMethodAsync("OnEvent", event_name, null);
+            }
+          });
         });
+  }
 
-        this.instance.addOverlay(c);
+  getOriginInstance = () => this.instance;
 
-        return c;
-    }
+  getCenter = () => this.instance.getCenter();
 
-    initAndAddMarker(marker) {
-        var m = new BMapGL.Marker(marker.point, {
-            offset: marker.offset,
-            rotation: marker.rotation,
-            title: marker.title
-        });
+  setZoom = (zoom) => this.instance.setZoom(zoom);
 
-        this.instance.addOverlay(m);
+  getZoom = () => this.instance.getZoom();
 
-        return m;
-    }
+  setMaxZoom = (maxZoom) => this.instance.setMaxZoom(maxZoom);
 
-    initAndAddLabel(label) {
-        var l = new BMapGL.Label(label.content, {
-            offset: label.offset,
-            position: label.position
-        });
+  setMinZoom = (minZoom) => this.instance.setMinZoom(minZoom);
 
-        this.instance.addOverlay(l);
+  enableScrollWheelZoom = () => this.instance.enableScrollWheelZoom();
 
-        return l;
-    }
+  disableScrollWheelZoom = () => this.instance.disableScrollWheelZoom();
 
-    initAndAddPolyline(polyline) {
-        if (polyline.points == null)
-            return null;
+  setMapType = (mapType) => this.instance.setMapType(mapType);
 
-        var pl = new BMapGL.Polyline(polyline.points, {
-            strokeColor: polyline.strokeColor,
-            strokeWeight: polyline.strokeWeight,
-            strokeOpacity: polyline.strokeOpacity,
-            strokeStyle: polyline.strokeStyle == 0 ? "solid" : "dashed",
-            geodesic: polyline.geodesic,
-            clip: polyline.clip
-        });
+  getMapType = () => this.instance.getMapType();
 
-        this.instance.addOverlay(pl);
+  setTrafficOn = () => this.instance.setTrafficOn();
 
-        return pl;
-    }
+  setTrafficOff = () => this.instance.setTrafficOff();
 
-    toBMapGLPoint = (point) => new BMapGL.Point(point.lng, point.lat);
+  setMapStyleV2 = (options) => this.instance.setMapStyleV2(options);
 
-    initAndAddPolygon(polygon) {
-        if (polygon.points == null)
-            return null;
+  panTo = (point) => this.instance.panTo(point);
 
-        var bmapPoints = [];
-        polygon.points.forEach(element => {
-            bmapPoints.push(toBMapGLPoint(element));
-        });
+  addOverlay = (overlay) => this.instance.addOverlay(overlay);
 
-        var pg = new BMapGL.Polygon(bmapPoints, {
-            strokeColor: polygon.strokeColor,
-            strokeWeight: polygon.strokeWeight,
-            strokeOpacity: polygon.strokeOpacity,
-            strokeStyle: polygon.strokeStyle == 0 ? "solid" : "dashed",
-            fillColor: polygon.fillColor,
-            fillOpacity: polygon.fillOpacity
-        });
+  removeOverlay = (overlay) => this.instance.removeOverlay(overlay);
 
-        this.instance.addOverlay(pg);
+  clearOverlays = () => this.instance.clearOverlays();
 
-        return pg;
-    }
+  addCircle(circle) {
+    var c = new BMapGL.Circle(circle.center, circle.radius, {
+      strokeColor: circle.strokeColor,
+      strokeWeight: circle.strokeWeight,
+      strokeOpacity: circle.strokeOpacity,
+      strokeStyle: circle.strokeStyle == 0 ? "solid" : "dashed",
+      fillColor: circle.fillColor,
+      fillOpacity: circle.fillOpacity
+    });
+
+    this.instance.addOverlay(c);
+
+    return c;
+  }
+
+  addMarker(marker) {
+    var m = new BMapGL.Marker(marker.point, {
+      offset: marker.offset,
+      rotation: marker.rotation,
+      title: marker.title
+    });
+
+    this.instance.addOverlay(m);
+
+    return m;
+  }
+
+  addLabel(label) {
+    var l = new BMapGL.Label(label.content, {
+      offset: label.offset,
+      position: label.position
+    });
+
+    this.instance.addOverlay(l);
+
+    return l;
+  }
+
+  addPolyline(polyline) {
+    if (polyline.points == null)
+      return null;
+
+    var pl = new BMapGL.Polyline(polyline.points, {
+      strokeColor: polyline.strokeColor,
+      strokeWeight: polyline.strokeWeight,
+      strokeOpacity: polyline.strokeOpacity,
+      strokeStyle: polyline.strokeStyle == 0 ? "solid" : "dashed",
+      geodesic: polyline.geodesic,
+      clip: polyline.clip
+    });
+
+    this.instance.addOverlay(pl);
+
+    return pl;
+  }
+
+  toBMapGLPoint = (point) => new BMapGL.Point(point.lng, point.lat);
+
+  addPolygon(polygon) {
+    if (polygon.points == null)
+      return null;
+
+    var bmapPoints = [];
+    polygon.points.forEach(element => {
+      bmapPoints.push(toBMapGLPoint(element));
+    });
+
+    var pg = new BMapGL.Polygon(bmapPoints, {
+      strokeColor: polygon.strokeColor,
+      strokeWeight: polygon.strokeWeight,
+      strokeOpacity: polygon.strokeOpacity,
+      strokeStyle: polygon.strokeStyle == 0 ? "solid" : "dashed",
+      fillColor: polygon.fillColor,
+      fillOpacity: polygon.fillOpacity
+    });
+
+    this.instance.addOverlay(pg);
+
+    return pg;
+  }
 }
 
 const init = (containerId, initArgs) => new BaiduMapProxy(containerId, initArgs);
