df2bfa00d82bacb996c720d014fbd979c4305e4c
capdiem <capdiem@live.com>
2023-02-21 09:42:37 +0800

    🐛 fix(DataTable): exception throwed when there is a custom header value (#320)

--- src/Component/BlazorComponent/Components/Data/ItemValue.cs
+++ src/Component/BlazorComponent/Components/Data/ItemValue.cs
@@ -4,49 +4,42 @@ namespace BlazorComponent
 {
     public class ItemValue<TItem>
     {
-        private Func<TItem, object> _factory;
+        private Func<TItem, object>? _factory;
 
         public ItemValue(string name)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
         }
 
-        public ItemValue(Func<TItem, object> factory)
-        {
-            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
-        }
-
         public string Name { get; }
 
-        public Func<TItem, object> Factory
+        public Func<TItem, object>? Factory
         {
             get
             {
-                if (_factory == null)
-                {
-                    try
-                    {
-                        var parameterExpression = Expression.Parameter(typeof(TItem), "item");
-                        var propertyExpression = Expression.Property(parameterExpression, Name);
-                        var valueExpression = Expression.Convert(propertyExpression, typeof(object));
+                if (_factory != null) return _factory;
 
-                        var lambdaExpression = Expression.Lambda<Func<TItem, object>>(valueExpression, parameterExpression);
-                        _factory = lambdaExpression.Compile();
-                    }
-                    catch (Exception ex)
-                    {
-                        //REVIEW:Is this ok?
-                        throw new InvalidOperationException($"Can not access property {Name} of {typeof(TItem).Name}", ex);
-                    }
+                try
+                {
+                    var parameterExpression = Expression.Parameter(typeof(TItem), "item");
+                    var propertyExpression = Expression.Property(parameterExpression, Name);
+                    var valueExpression = Expression.Convert(propertyExpression, typeof(object));
+
+                    var lambdaExpression = Expression.Lambda<Func<TItem, object>>(valueExpression, parameterExpression);
+                    _factory = lambdaExpression.Compile();
+                }
+                catch (Exception)
+                {
+                    _factory = null;
                 }
 
                 return _factory;
             }
         }
 
-        public object Invoke(TItem item)
+        public object? Invoke(TItem item)
         {
-            return Factory.Invoke(item);
+            return Factory?.Invoke(item);
         }
 
         public static implicit operator string(ItemValue<TItem> itemValue) => itemValue.Name;
