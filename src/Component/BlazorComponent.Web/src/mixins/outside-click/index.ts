a092627c51075f9a2cc5fb7ca2aae53f978fdb20
capdiem <capdiem@live.com>
2023-03-08 16:09:40 +0800

    🐛 fix(outside-click): should know the last mousedown was outside

--- src/Component/BlazorComponent.Web/src/mixins/outside-click/index.ts
+++ src/Component/BlazorComponent.Web/src/mixins/outside-click/index.ts
@@ -1,53 +1,63 @@
 class OutsideClick {
   dotNetHelper: DotNet.DotNetObject;
   listener: (e: MouseEvent & KeyboardEvent & FocusEvent) => void;
+  mousedownListener: (e: MouseEvent) => void;
   excludedSelectors: string[];
+  lastMousedownWasOutside: boolean = true;
 
   constructor(dotNetHelper: DotNet.DotNetObject, excludedSelectors: string[]) {
     this.dotNetHelper = dotNetHelper;
     this.excludedSelectors = excludedSelectors;
   }
 
-  genListener() {
+  genListeners() {
     this.listener = (e) => {
-      if (
-        this.excludedSelectors.some((selector) => {
-          const el = document.querySelector(selector);
-          if (!el) return false;
+      if (this.checkEvent(e)) return;
 
-          return el.contains(e.target as HTMLElement);
-        })
-      ) {
-        return;
+      if (this.lastMousedownWasOutside) {
+        this.dotNetHelper.invokeMethodAsync("OnOutsideClick");
       }
+    };
 
-      this.dotNetHelper.invokeMethodAsync("OnOutsideClick");
+    this.mousedownListener = (e) => {
+      this.lastMousedownWasOutside = !this.checkEvent(e);
     };
   }
 
-  addListener() {
-    this.genListener();
+  addListeners() {
+    this.genListeners();
     document.addEventListener("click", this.listener, true);
+    document.addEventListener("mousedown", this.mousedownListener, true);
   }
 
-  removeListener() {
+  removeListeners() {
     document.removeEventListener("click", this.listener);
+    document.removeEventListener("mousedown", this.mousedownListener);
   }
 
   resetListener() {
-    this.removeListener();
-    this.addListener();
+    this.removeListeners();
+    this.addListeners();
   }
 
   updateExcludeSelectors(selectors: string[]) {
     this.excludedSelectors = selectors;
+  }
+
+  checkEvent(e: MouseEvent) {
+    return this.excludedSelectors.some((selector) => {
+      const el = document.querySelector(selector);
+      if (!el) return false;
+
+      return el.contains(e.target as HTMLElement);
+    });
   }
 }
 
 function init(dotNetHelper: DotNet.DotNetObject, excludeSelectors: string[]) {
   var instance = new OutsideClick(dotNetHelper, excludeSelectors);
 
-  instance.addListener();
+  instance.addListeners();
 
   return instance;
 }
