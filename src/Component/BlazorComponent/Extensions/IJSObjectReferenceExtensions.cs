2e4b9a18a26e0df17015f0447167146ca7c7c0e5
Ximer Deng <85274323+msnoshain@users.noreply.github.com>
2023-03-09 15:45:29 +0800

    🆕 feat(BaiduMap): Support overlays and events (#310)
    * Add constructCircle method
    
    * Add constructMarker method
    
    * Add constructPolyline method; Add constructLabel method; Add an overload method for TryInvokeVoidAsync
    
    * Add constructPolygon method
    
    * Fix polygon construction
    
    * Reduce nesting
    
    * Add null check
    
    * Add JSObjectReferenceProxy
    
    * Delete my JSObjRefProxy
    
    * Clear my imports
    
    * Rename functions
    
    * Support events
    
    * Add overlays directly to map after initializing them
    
    * Improve code indent
    
    * Remove blank lines
    
    * Remove unused methods
    
    * Complete initArgs
    
    * Fix a bug
    
    * Add "contains" method
    
    * Add "destroy" method
    
    * Revert "Add "destroy" method"
    
    This reverts commit df46bc24f21eabf70d3c6e92bdaf03f08fe56e23.

--- src/Component/BlazorComponent/Extensions/IJSObjectReferenceExtensions.cs
+++ src/Component/BlazorComponent/Extensions/IJSObjectReferenceExtensions.cs
@@ -7,11 +7,17 @@ namespace BlazorComponent
         public static async ValueTask TryInvokeVoidAsync(this IJSObjectReference? jsObjectReference, string identifier, params object?[]? args)
         {
             if (jsObjectReference is null)
-            {
                 return;
-            }
-
+            
             await jsObjectReference.InvokeVoidAsync(identifier, args);
+        }
+
+        public static async ValueTask<TValue?> TryInvokeAsync<TValue>(this IJSObjectReference? jsObjectReference, string identifier, params object?[]? args)
+        {
+            if (jsObjectReference is null)
+                return default;
+
+            return await jsObjectReference.InvokeAsync<TValue>(identifier, args);
         }
     }
 }
