6dd7457beb2ebc92aeaca2abb68150afaf1d3cb6
capdiem <capdiem@live.com>
2023-03-23 17:44:01 +0800

    🆕 feat(logger): Add protected Logger property

--- src/Component/BlazorComponent/_Imports.cs
+++ src/Component/BlazorComponent/_Imports.cs
@@ -3,5 +3,6 @@ global using System.Diagnostics.CodeAnalysis;
 global using BlazorComponent.Attributes;
 global using BlazorComponent.Web;
 global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.Extensions.Logging;
 global using OneOf;
 global using Microsoft.JSInterop;
