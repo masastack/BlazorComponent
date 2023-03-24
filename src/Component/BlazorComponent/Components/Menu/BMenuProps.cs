13c7cadf21d50fb80d1b1fa728672589ad7bd0d0
capdiem <capdiem@live.com>
2023-03-24 11:43:05 +0800

    🆕 feat(MenuProps): add OffsetOverflow and Transition properties

--- src/Component/BlazorComponent/Components/Menu/BMenuProps.cs
+++ src/Component/BlazorComponent/Components/Menu/BMenuProps.cs
@@ -3,7 +3,7 @@
     public class BMenuProps
     {
         public bool Auto { get; set; }
-        
+
         public bool Bottom { get; set; }
 
         public bool CloseOnClick { get; set; }
@@ -34,12 +34,14 @@
 
         public bool OffsetY { get; set; }
 
+        public bool OffsetOverflow { get; set; }
+
         public bool OpenOnClick { get; set; }
 
         public bool Right { get; set; }
 
         public bool Top { get; set; }
 
-        public bool Visible { get; set; }
+        public string? Transition { get; set; }
     }
 }
