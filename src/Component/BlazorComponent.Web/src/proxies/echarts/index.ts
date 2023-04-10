7f7a8e740e573dfc4abc4a55222aec0d5d55b4cb
Kogel <42330679+kogel-net@users.noreply.github.com>
2023-04-10 14:55:48 +0800

    fix(ECharts): Special Character Conversion (#369)
    * fix:update masa.blazor fix-issues-283
    
    * fix:index.ts code format

--- src/Component/BlazorComponent.Web/src/proxies/echarts/index.ts
+++ src/Component/BlazorComponent.Web/src/proxies/echarts/index.ts
@@ -46,6 +46,14 @@ class EChartsProxy {
     this.instance.setOption(option, notMerge, lazyUpdate);
   }
 
+  setJsonOption(
+    option: any,
+    notMerge: boolean = false,
+    lazyUpdate: boolean = false
+  ) {
+    this.instance.setOption(eval("option=" + option), notMerge, lazyUpdate);
+  }
+  
   resize(opts?: EChartsResizeOption) {
     this.instance.resize(opts);
   }
