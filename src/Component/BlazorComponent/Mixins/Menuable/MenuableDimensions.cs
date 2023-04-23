8e4d3d6b1f5f956fdb696376822e16edfe9c629a
capdiem <capdiem@live.com>
2023-04-23 11:55:48 +0800

    🐛 fix(Menu): NullReferenceException (#389)

--- src/Component/BlazorComponent/Mixins/Menuable/MenuableDimensions.cs
+++ src/Component/BlazorComponent/Mixins/Menuable/MenuableDimensions.cs
@@ -2,8 +2,8 @@
 {
     public class MenuableDimensions
     {
-        public MenuablePosition Activator { get; set; } = new MenuablePosition();
-        public MenuablePosition Content { get; set; } = new MenuablePosition();
+        public MenuablePosition Activator { get; set; } = new();
+        public MenuablePosition Content { get; set; } = new();
         public double RelativeYOffset { get; set; }
         public double OffsetParentLeft { get; set; }
     }
