b7bb4ad80246614b1df9cb7011c5426af8605942
capdiem <capdiem@live.com>
2023-05-19 13:45:14 +0800

    docs: Support for dark theme (#406)
    * 🚧 feat: wip
    
    * 🚧 feat: wip
    
    * 🚧 feat: wip
    
    * Use preview lang version

--- src/Component/BlazorComponent/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Component/BlazorComponent/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 services.AddOptions<BlazorComponentOptions>().Configure(optionsAction);
             }
 
+            services.TryAddScoped<LocalStorage>();
             services.TryAddScoped<DomEventJsInterop>();
             services.TryAddScoped<Document>();
             services.TryAddScoped(serviceProvider => new Window(serviceProvider.GetRequiredService<Document>()));
