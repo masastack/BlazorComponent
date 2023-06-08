30f732b5a8e279155c9137686a89f9b2b72c2e27
capdiem <capdiem@live.com>
2023-06-08 10:49:48 +0800

    🆕 feat(ExpansionPanel): support for lazy loading the DOM of content (#428)
    * 🆕 feat(ExpansionPanel): support for lazy loading the DOM of content
    
    * 🆕 feat(ExpansionPanelContent): eager: force to render on init

--- src/Component/BlazorComponent/Components/ExpansionPanels/BExpansionPanelContent.razor.cs
+++ src/Component/BlazorComponent/Components/ExpansionPanels/BExpansionPanelContent.razor.cs
@@ -6,6 +6,39 @@
         public BExpansionPanel? ExpansionPanel { get; set; }
 
         [Parameter]
+        public bool Eager { get; set; }
+
+        [Parameter]
         public RenderFragment? ChildContent { get; set; }
+
+        private bool _isBooted;
+        private bool _booting;
+        private bool _isActive;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await base.OnParametersSetAsync();
+
+            if (ExpansionPanel != null)
+            {
+                if ((_isBooted && _booting == false) || Eager)
+                {
+                    _isActive = ExpansionPanel.InternalIsActive;
+                }
+                else if (ExpansionPanel.Booted)
+                {
+                    _isBooted = true;
+                    _booting = true;
+
+                    if (ExpansionPanel.InternalIsActive)
+                    {
+                        await Task.Delay(16);
+
+                        _booting = false;
+                        _isActive = true;
+                    }
+                }
+            }
+        }
     }
 }
