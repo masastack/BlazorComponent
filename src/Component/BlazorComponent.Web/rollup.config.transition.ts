2cd4ce2cb4a014d7cb4508693aa180275ffd7284
capdiem <capdiem@live.com>
2023-07-13 14:05:25 +0800

    🚧 feat: wip

---
+++ src/Component/BlazorComponent.Web/rollup.config.transition.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "rollup";
+import { terser } from "rollup-plugin-terser";
+
+import typescript from "@rollup/plugin-typescript";
+
+export default defineConfig({
+  input: "./src/transitions/index.ts",
+  output: [
+    {
+      file: "../../../../Masa.Blazor.Playground/wwwroot/transition.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript(), terser()],
+  watch: {
+    exclude: "node_modules/**",
+  },
+});
--- src/Component/BlazorComponent.Web/src/transitions/index.ts
+++ src/Component/BlazorComponent.Web/src/transitions/index.ts
@@ -1,6 +1,9 @@
 class Transition {
+  dotnetHelper: DotNet.DotNetObject;
   name: string;
   el: HTMLElement;
+  leaveAbsolute: boolean;
+
   enterClass: string;
   enterActiveClass: string;
   enterToClass: string;
@@ -8,8 +11,16 @@ class Transition {
   leaveActiveClass: string;
   leaveToClass: string;
 
-  constructor(name: string, elOrSelector: HTMLElement | string) {
+  constructor(
+    dotnetHelper: DotNet.DotNetObject,
+    name: string,
+    elOrSelector: HTMLElement | string,
+    leaveAbsolute: boolean
+  ) {
+    this.dotnetHelper = dotnetHelper;
     this.name = name;
+    this.leaveAbsolute = leaveAbsolute;
+
     this.enterClass = `${this.name}-enter`;
     this.enterActiveClass = `${this.name}-enter-active`;
     this.enterToClass = `${this.name}-enter-to`;
@@ -24,13 +35,23 @@ class Transition {
     }
   }
 
-  onEnter() {
+  onEnter(newEl: HTMLElement) {
+    if (newEl) {
+      this.el = newEl;
+    }
+
+    console.log("this.el", this.el);
+
     this.addTransitionClass(this.el, this.enterClass);
     this.addTransitionClass(this.el, this.enterActiveClass);
+
+    console.log("add enter from class", this.el.className);
 
     const resolve = () => {
       this.removeTransitionClass(this.el, this.enterToClass);
       this.removeTransitionClass(this.el, this.enterActiveClass);
+
+      console.log("remove enter to class", this.el.className);
     };
 
     requestAnimationFrame(() => {
@@ -38,25 +59,103 @@ class Transition {
         this.removeTransitionClass(this.el, this.enterClass);
         this.addTransitionClass(this.el, this.enterToClass);
 
+        console.log("add enter to class", this.el.className);
+
+        this.el.addEventListener("transitionend", resolve);
+      });
+    });
+  }
+
+  delay(cb, ms) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(cb), ms);
+    });
+  }
+
+  onEnterTo(newEl: HTMLElement) {
+    if (newEl) {
+      this.el = newEl;
+    }
+
+    const resolve = () => {
+      this.removeTransitionClass(this.el, this.enterToClass);
+      this.removeTransitionClass(this.el, this.enterActiveClass);
+
+      console.log("remove enter to class", this.el.className);
+    };
+
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        console.log("this.el", this.el, this.el.className, this.el.classList);
+        this.removeTransitionClass(this.el, this.enterClass);
+        this.addTransitionClass(this.el, this.enterToClass);
+
+        console.log("add enter to class", this.el.className);
+
         this.el.addEventListener("transitionend", resolve);
       });
     });
   }
 
   onLeave() {
-    const resolve = () => {
-      this.removeTransitionClass(this.el, this.leaveToClass);
-      this.removeTransitionClass(this.el, this.leaveActiveClass);
-    };
+    console.log("this.leaveAbsolute", this.leaveAbsolute);
+    if (this.leaveAbsolute) {
+      const { offsetTop, offsetLeft, offsetWidth, offsetHeight } = this.el;
+
+      this.el["_transitionInitialStyles"] = {
+        position: this.el.style.position,
+        top: this.el.style.top,
+        left: this.el.style.left,
+        width: this.el.style.width,
+        height: this.el.style.height,
+      };
+
+      console.log("this.el.offsetLeft", this.el.offsetLeft);
+
+      this.el.style.position = "absolute";
+      this.el.style.top = offsetTop + "px";
+      this.el.style.left = offsetLeft + "px";
+      this.el.style.width = offsetWidth + "px";
+      this.el.style.height = offsetHeight + "px";
+
+      console.log("this.el.style.left", this.el.style.left);
+    }
 
     this.addTransitionClass(this.el, this.leaveClass);
     this.addTransitionClass(this.el, this.leaveActiveClass);
 
+    console.log("this.el.className", this.el.className);
+
+    const resolve = async () => {
+      console.log("OnTransitionEnd.....");
+      // It is called here rather than after method removeTransitionClass
+      // because the js interop call is affected by the network latency.
+      await this.dotnetHelper.invokeMethodAsync("OnTransitionEnd");
+
+      console.log('before removeTransitionClass ->', this.el.className)
+
+      this.removeTransitionClass(this.el, this.leaveToClass);
+      this.removeTransitionClass(this.el, this.leaveActiveClass);
+
+      console.log('removeTransitionClass this.el.className ->', this.el.className)
+
+      if (this.el && this.el["_transitionInitialStyles"]) {
+        const { position, top, left, width, height } = this.el["_transitionInitialStyles"];
+        delete this.el["_transitionInitialStyles"];
+        this.el.style.position = position || "";
+        this.el.style.top = top || "";
+        this.el.style.left = left || "";
+        this.el.style.width = width || "";
+        this.el.style.height = height || "";
+      }
+    };
     requestAnimationFrame(() => {
       requestAnimationFrame(() => {
+        console.log("requestAnimationFrame...");
         this.removeTransitionClass(this.el, this.leaveClass);
         this.addTransitionClass(this.el, this.leaveToClass);
 
+        // todo: transitionend once?
         this.el.addEventListener("transitionend", resolve);
       });
     });
@@ -71,8 +170,18 @@ class Transition {
   }
 }
 
-function init(name: string, elOrSelector: HTMLElement | string) {
-  const transitionEl = new Transition(name, elOrSelector);
+function init(
+  dotnetHelper: DotNet.DotNetObject,
+  name: string,
+  elOrSelector: HTMLElement | string,
+  leaveAbsolute: boolean
+) {
+  const transitionEl = new Transition(
+    dotnetHelper,
+    name,
+    elOrSelector,
+    leaveAbsolute
+  );
   return transitionEl;
 }
 
