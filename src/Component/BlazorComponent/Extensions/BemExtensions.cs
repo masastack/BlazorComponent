6d25c9010b3c6619d0ec6d93b095d77ce3041a8e
capdiem <capdiem@live.com>
2023-08-03 18:32:39 +0800

    add method extensions of Bem

---
+++ src/Component/BlazorComponent/Extensions/BemExtensions.cs
@@ -0,0 +1,64 @@
+﻿using BemIt;
+
+namespace BlazorComponent;
+
+public static class BemExtensions
+{
+    public static IBem AddTheme(this IBem bem, bool isDark, bool exDark = false)
+    {
+        var dark = isDark || exDark;
+        bem.Add(dark ? "theme--dark" : "theme--light");
+        return bem;
+    }
+
+    public static IBem AddBackgroundColor(this IBem bem, string? color, bool apply = true)
+    {
+        return bem.AddColor(color, false, apply);
+    }
+
+    public static IBem AddTextColor(this IBem bem, string? color, bool apply = true)
+    {
+        return bem.AddColor(color, true, apply);
+    }
+
+    public static IBem AddColor(this IBem bem, string? color, bool isText, bool apply = true)
+    {
+        if (string.IsNullOrEmpty(color) || color.StartsWith("#") || color.StartsWith("rgb"))
+        {
+            return bem;
+        }
+
+        if (isText)
+        {
+            var colors = color.Split(" ");
+            var firstColor = colors[0];
+
+            if (apply)
+            {
+                bem.Add($"{firstColor}--text");
+            }
+
+            if (colors.Length == 2)
+            {
+                // TODO: 是否需要正则表达式验证格式
+                // {darken|lighten|accent}-{1|2}
+
+                var secondColor = colors[1];
+
+                if (apply)
+                {
+                    bem.Add($"text--{secondColor}");
+                }
+            }
+        }
+        else
+        {
+            if (apply)
+            {
+                bem.Add(color);
+            }
+        }
+
+        return bem;
+    }
+}
