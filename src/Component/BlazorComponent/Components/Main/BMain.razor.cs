0c8ca96c431e2e949269d075d85b871f06fcbf53
capdiem <capdiem@live.com>
2023-09-18 14:39:52 +0800

    remove unused code

--- src/Component/BlazorComponent/Components/Main/BMain.razor.cs
+++ src/Component/BlazorComponent/Components/Main/BMain.razor.cs
@@ -3,9 +3,4 @@
 public abstract partial class BMain : BDomComponentBase
 {
     [Parameter] public RenderFragment? ChildContent { get; set; }
-
-    /// <summary>
-    /// Determines if the DOM element is ready to be displayed.
-    /// </summary>
-    protected virtual bool IsBooted => true;
 }
