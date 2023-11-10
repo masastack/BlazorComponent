e2b99245c6253c2cd44af8a44e47c4d464c64174
capdiem <capdiem@live.com>
2023-11-10 10:30:26 +0800

    🐛 fix(Transition): transition may not finish when the delay was high (#503)

--- src/Component/BlazorComponent/Components/Transition/TransitionJsInvoker.cs
+++ src/Component/BlazorComponent/Components/Transition/TransitionJsInvoker.cs
@@ -10,6 +10,8 @@ public class TransitionJsInvoker : IAsyncDisposable
 
     public TransitionJsInvoker(IJSRuntime js) => _js = js;
 
+    public bool Registered { get; private set; } 
+
     public async Task Init(Func<string, LeaveEnter, Task> onTransitionEnd, Func<string, LeaveEnter, Task> onTransitionCancel)
     {
         _module = await _js.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorComponent/js/transition.js");
@@ -21,6 +23,7 @@ public class TransitionJsInvoker : IAsyncDisposable
         if (_module is not null && !_isDisposed)
         {
             await _module.InvokeVoidAsync("registerTransitionEvents", token, reference, _objRef);
+            Registered = true;
         }
     }
 
