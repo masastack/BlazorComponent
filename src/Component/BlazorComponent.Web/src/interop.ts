2d86e2ac896d64c6a886165fb7387716aa81eb36
capdiem <capdiem@live.com>
2023-12-26 15:53:36 +0800

    🐛 fix(js-setCookie): the value may be null

--- src/Component/BlazorComponent.Web/src/interop.ts
+++ src/Component/BlazorComponent.Web/src/interop.ts
@@ -1292,7 +1292,7 @@ export function setCookie(name, value) {
   var Days = 30;
   var exp = new Date();
   exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-  document.cookie = `${name}=${escape(value.toString())};path=/;expires=${exp.toUTCString()};domain=${domain}`;
+  document.cookie = `${name}=${escape(value?.toString())};path=/;expires=${exp.toUTCString()};domain=${domain}`;
 }
 
 export function getCookie(name) {
