87dda7dea23eea5bc0f17cdbc2a35c345ab24f54
capdiem <capdiem@live.com>
2024-01-02 13:24:57 +0800

    🆕 feat(Theme): add OnPrimary, OnSecondary, OnAccent, OnError, Surface and OnSurface roles

--- src/Component/BlazorComponent/Services/ThemeOptions.cs
+++ src/Component/BlazorComponent/Services/ThemeOptions.cs
@@ -5,18 +5,33 @@ public class ThemeOptions
     public string? CombinePrefix { get; set; }
 
     public string? Primary { get; set; }
+    
+    public string? OnPrimary { get; set; }
 
     public string? Secondary { get; set; }
+    
+    public string? OnSecondary { get; set; }
 
+    /// <summary>
+    /// Teritary
+    /// </summary>
     public string? Accent { get; set; }
 
+    public string? OnAccent { get; set; }
+
     public string? Error { get; set; }
+    
+    public string? OnError { get; set; }
 
     public string? Info { get; set; }
 
     public string? Success { get; set; }
 
     public string? Warning { get; set; }
+    
+    public string? Surface { get; set; }
+    
+    public string? OnSurface { get; set; }
 
     public Dictionary<string, string> UserDefined { get; } = new();
 }
