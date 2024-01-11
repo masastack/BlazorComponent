18b8d29523bac961a0a8c59c49190e5bce55c7a1
capdiem <capdiem@live.com>
2024-01-11 14:17:35 +0800

    refactor(Alert): move to MASA.Blazor (#537)
    * ♻ refactor: working on move alert to masablazor
    
    * update

--- src/Component/BlazorComponent/Abstracts/Builder/BemCssBuilder.cs
+++ src/Component/BlazorComponent/Abstracts/Builder/BemCssBuilder.cs
@@ -3,26 +3,25 @@ namespace BlazorComponent.Abstracts;
 public class BemCssBuilder : CssBuilder
 {
     private readonly IBlockOrElement _blockOrElement;
-    private Func<IBlockOrElement, IBem> _builder;
+    private Func<IBlockOrElement, IBem>? _builder;
 
     public BemCssBuilder(IBlockOrElement blockOrElement, object? data = null)
     {
         _blockOrElement = blockOrElement;
-        _builder = be => be;
 
         Data = data;
     }
 
     public CssBuilder Modifiers(Func<IBlockOrElement, IBem> bemBuilder)
     {
-        _builder = bemBuilder;
+        _builder ??= bemBuilder;
         return this;
     }
 
     protected override List<string?> GetClassNames()
     {
         var classNames = base.GetClassNames();
-        classNames.Insert(0, _builder.Invoke(_blockOrElement).Build());
+        classNames.Add(_builder == null ? _blockOrElement.Name : _builder.Invoke(_blockOrElement).Build());
         return classNames;
     }
 }
--- src/Component/BlazorComponent/Extensions/BemExtensions.cs
+++ src/Component/BlazorComponent/Extensions/BemExtensions.cs
@@ -1,19 +1,92 @@
-﻿using BemIt;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using BemIt;
 
 namespace BlazorComponent;
 
 public static class BemExtensions
 {
-    public static IBem AddTheme(this IBem bem, bool isDark, bool exDark = false)
+    public static Modifier AddOneOf(this Modifier modifier, 
+        bool modifier1, bool modifier2,
+        [CallerArgumentExpression("modifier1")] string name1 = "",
+        [CallerArgumentExpression("modifier2")] string name2 = "")
     {
-        var dark = isDark || exDark;
-        bem.AddClass(dark ? "theme--dark" : "theme--light");
+        if (modifier1)
+        {
+            modifier.Add(name1);
+        }
+        else if (modifier2)
+        {
+            modifier.Add(name2);
+        }
+
+        return modifier;
+    }
+
+    public static Modifier Add(this Modifier modifier, Enum @enum, bool apply = true)
+    {
+        if (apply)
+        {
+            return modifier.Add(@enum);
+        }
+
+        return modifier;
+    }
+    
+    public static IBem AddTheme(this IBem bem, bool isDark, bool isIndependent = false)
+    {
+        bem.AddClass(isDark ? "theme--dark" : "theme--light");
+
+        if (isIndependent)
+        {
+            bem.AddClass("theme--independent");
+        }
+
         return bem;
     }
 
     public static IBem AddBackgroundColor(this IBem bem, string? color, bool apply = true)
     {
         return bem.AddColor(color, false, apply);
+    }
+    
+    public static IBem AddElevation(this IBem bem, StringNumber? elevation)
+    {
+        if (elevation != null)
+        {
+            bem.AddClass($"elevation-{elevation}");
+        }
+        
+        return bem;
+    }
+    
+    public static IBem AddRounded(this IBem bem, StringBoolean? rounded, bool tile)
+    {
+        if (tile)
+        {
+            bem.AddClass("rounded-0");
+        }
+        else
+        {
+            if (rounded != null)
+            {
+                if (rounded.IsT0)
+                {
+                    var values = rounded.AsT0.Split(' ');
+
+                    foreach (var val in values)
+                    {
+                        bem.AddClass($"rounded-{val}");
+                    }
+                }
+                else if (rounded.IsT1 && rounded.AsT1)
+                {
+                    bem.AddClass("rounded");
+                }
+            }
+        }
+
+        return bem;
     }
 
     public static IBem AddTextColor(this IBem bem, string? color, bool apply = true)
