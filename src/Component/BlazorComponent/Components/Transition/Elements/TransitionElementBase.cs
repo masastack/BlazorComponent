78dbc919cf00e75bb3292353c0644be88e9f964d
capdiem <capdiem@live.com>
2024-02-28 11:49:45 +0800

    feat: Add Sortable component to replace DragZone component (#561)
    * 🚧 feat: working on Sortable component
    
    * Update Element
    
    * wip
    
    * update
    
    * update

--- src/Component/BlazorComponent/Components/Transition/Elements/TransitionElementBase.cs
+++ src/Component/BlazorComponent/Components/Transition/Elements/TransitionElementBase.cs
@@ -1,15 +1,49 @@
 ﻿using System.Text;
+using Microsoft.AspNetCore.Components.Rendering;
 
 namespace BlazorComponent;
 
 public abstract class TransitionElementBase : Element
 {
+    private ElementReference? _reference;
+
+    protected bool ElementReferenceChanged { get; set; }
+
     internal abstract TransitionState CurrentState { get; set; }
 
     /// <summary>
     /// The dom information about the transitional element.
     /// </summary>
     internal BlazorComponent.Web.Element? ElementInfo { get; set; }
+
+    public ElementReference Reference
+    {
+        get => _reference ?? new ElementReference();
+        protected set
+        {
+            if (_reference.HasValue && _reference.Value.Id != value.Id)
+            {
+                ElementReferenceChanged = true;
+            }
+
+            _reference = value;
+        }
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, (Tag ?? "div"));
+        builder.AddMultipleAttributes(1, AdditionalAttributes);
+        builder.AddAttribute(2, "class", ComputedClass);
+        builder.AddAttribute(3, "style", ComputedStyle);
+        builder.AddContent(4, ChildContent);
+        builder.AddElementReferenceCapture(5, reference =>
+        {
+            Reference = reference;
+            ReferenceCaptureAction?.Invoke(reference);
+        });
+        builder.CloseElement();
+    }
 }
 
 #if NET6_0
