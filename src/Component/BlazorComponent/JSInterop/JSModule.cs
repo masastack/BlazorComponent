50c11bc32d023cd65078397855777d5bd15593f9
capdiem <capdiem@live.com>
2024-03-01 16:48:10 +0800

    fix(JSModule): ignore InvalidOperationException when prerendering (#566)
    * 🐛 fix(JSModule): catch InvalidOperationException
    
    * catch InvalidOperationException
    
    * update

--- src/Component/BlazorComponent/JSInterop/JSModule.cs
+++ src/Component/BlazorComponent/JSInterop/JSModule.cs
@@ -74,15 +74,17 @@ public abstract class JSModule : IAsyncDisposable
             {
                 // ignored
             }
-            catch (JSException e)
+            // HACK: remove this after https://github.com/dotnet/aspnetcore/issues/52119 is fixed
+            catch (JSException e) when (e.Message.Contains("has it been disposed", StringComparison.InvariantCulture)
+                                        && (OperatingSystem.IsWindows() || OperatingSystem.IsAndroid() ||
+                                            OperatingSystem.IsIOS()))
             {
-                // HACK: remove this after https://github.com/dotnet/aspnetcore/issues/52119 is fixed
-                if (e.Message.Contains("has it been disposed") && (OperatingSystem.IsWindows() || OperatingSystem.IsAndroid() || OperatingSystem.IsIOS()))
-                {
-                    return;
-                }
-
-                throw;
+                // ignored
+            }
+            catch (InvalidOperationException e) when (e.Message.Contains("prerendering",
+                                                          StringComparison.InvariantCulture))
+            {
+                // ignored
             }
         }
 
