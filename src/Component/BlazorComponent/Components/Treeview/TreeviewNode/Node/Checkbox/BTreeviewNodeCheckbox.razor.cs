5179193419d9b4007bee49385c23f73aa4d8a114
capdiem <capdiem@live.com>
2024-04-16 10:14:06 +0800

    🚚 refactor(Treeview): move code to Masa.Blazor

--- src/Component/BlazorComponent/Components/Treeview/TreeviewNode/Node/Checkbox/BTreeviewNodeCheckbox.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorComponent
-{
-    public partial class BTreeviewNodeCheckbox<TItem, TKey, TTreeviewNode> where TTreeviewNode : ITreeviewNode<TItem, TKey>
-    {
-        public string? ComputedIcon => Component.ComputedIcon;
-
-        public string Name => "checkbox";
-    }
-}
--- src/Component/BlazorComponent/Components/Treeview/TreeviewNode/Node/Content/BTreeviewNodeContent.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorComponent
-{
-    public partial class BTreeviewNodeContent<TItem, TKey, TTreeviewNode> where TTreeviewNode : ITreeviewNode<TItem, TKey>
-    {
-
-    }
-}
